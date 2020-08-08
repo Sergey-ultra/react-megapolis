04e6246d70af65a74acd824a1fa2ee232d267421
Sergey-ultra <maasa@list.ru>
2020-08-09 00:19:14 +0400

    Updates

---
+++ precache-manifest.c04bb3f624a836032dd3b13d50448d42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62642395272faba876d49fe73ae79939",
+    "url": "/react-megapolis/index.html"
+  },
+  {
+    "revision": "b86d0b6126868083ab50",
+    "url": "/react-megapolis/static/css/main.eedf99c6.chunk.css"
+  },
+  {
+    "revision": "77f96ea7c3354109609f",
+    "url": "/react-megapolis/static/js/2.3958a0d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-megapolis/static/js/2.3958a0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86d0b6126868083ab50",
+    "url": "/react-megapolis/static/js/main.d2a20c2a.chunk.js"
+  },
+  {
+    "revision": "cf57d5a12359275a43b6",
+    "url": "/react-megapolis/static/js/runtime-main.712650d6.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/react-megapolis/static/media/1_9EBHIOzhE1XfMYoKz1JcsQ.26ef8bb4.gif"
+  }
+]);
